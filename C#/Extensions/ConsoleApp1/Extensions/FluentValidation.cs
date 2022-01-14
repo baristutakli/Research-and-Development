08eef27f6800b0bf6dd1a101acbff270fe8bc34b
kodluyoruz <baristutakli@hotmail.com>
2022-01-14 19:20:51 +0300

    C# Extension Method

---
+++ C#/Extensions/ConsoleApp1/Extensions/FluentValidation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Extensions
+{
+    public static class FluentValidation
+    {
+        public static bool IsNotNullAndNotEmpty(this string email)
+        {
+            
+            return email is not null && email != "";
+        }
+
+        public static bool IsLongEnough(this string email)
+        {
+            return email.Length > 15 ? true:false;
+        }
+        public static bool IsStrongEnough(this string email)
+        {
+            bool IsStrong = true;
+            List<string> patterns= new List<string> { @"[A-Z]", @"[a-z]", @"[0-9]", @"\W" };
+          
+            MatchCollection regex;
+            patterns.ForEach(pattern =>
+            {
+                regex = Regex.Matches(email, pattern);
+                if (regex.Count==0)
+                {
+                    IsStrong = false;
+                }
+            }
+
+            );
+
+            return email.IsLongEnough() && IsStrong ? true : false;
+
+
+        }
+    }
+}
---
+++ C#/Extensions/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp1.Extensions;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // How to use c# extension method
+          
+            string email= "asdfg@hotmail.com";
+            string password="1234567gcyh89?AgE-";
+            Console.WriteLine($"{email} is not NULL and not EMPTY: " + email.IsNotNullAndNotEmpty());
+            Console.WriteLine($"{password} is not NULL and not EMPTY: " + password.IsNotNullAndNotEmpty());
+            Console.WriteLine("IsLongEnough: " + password.IsLongEnough());
+            Console.WriteLine("IsStrongEnough: " + password.IsStrongEnough());
+        }
+    }
+}
