7764e267569b5dbfa76532f66dc2314580150ae1
kodluyoruz <baristutakli@hotmail.com>
2022-01-21 20:31:09 +0300

    Learned Observer pattern and Attributes

---
+++ C#/LearnLinQ/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLinQ
+{
+    public static class Extensions
+    {
+        public delegate bool FindMessage(string message);
+        public static List<string> Find(this IEnumerable<Message> source, FindMessage del)
+        {
+            List<string> newList = new List<string>();
+            foreach (var item in source)
+            {
+                if (del(item.MessageDetail))
+                {
+                    newList.Add(item.MessageDetail);
+                }
+            }
+           return newList;
+        }
+
+
+        // Func<Tinput,Toutput> => TInput: typeof input. TOutput: typeof output 
+
+        public static IEnumerable<T> CustomExtension<T>(this IEnumerable<T> data, Func<T, bool> predicate)
+        {
+            //List<string> newList = new List<string>();
+            foreach (T value in data)
+            {
+                if (predicate(value)) yield return value;
+            }
+        }
+
+    }}
---
+++ C#/LearnLinQ/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace LearnLinQ
+{
+    public  class Message
+    {
+        public int Id { get; set; }
+        public string MessageDetail { get; set; }
+    }
+}
