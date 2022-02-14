7ffa97e576321c66f41d770c83ad666c980f15bd
kodluyoruz <baristutakli@hotmail.com>
2022-02-14 14:06:02 +0300

    MongoDb Api

---
+++ BookStoreApi/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApi.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
