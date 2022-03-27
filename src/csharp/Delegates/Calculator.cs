49f0a9e369e6ab140cdfcd07e5a95564e293ce3a
kodluyoruz <baristutakli@hotmail.com>
2022-03-27 08:15:54 +0300

    Delegates

---
+++ src/csharp/Delegates/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class Calculator
+    {
+      
+        
+        public void Addition(int firstNumber,int secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} + {secondNumber}: {firstNumber + secondNumber}");
+        }
+
+        public int Substraction(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine("sub");
+            return firstNumber + secondNumber;
+        }
+
+        public int Multiplication(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public int Division(int firstNumber, int secondNumber)
+        {
+            return secondNumber != 0 ? firstNumber / secondNumber : throw new InvalidOperationException("Zero division!!!");
+        }
+
+    }
+}
+
+
+
