aef392f70d1470b148796e0327f1f9f09c5c8099
kodluyoruz <baristutakli@hotmail.com>
2022-03-28 20:28:54 +0300

    Initialized controller

---
+++ src/NOSQL/MongoDBProject/Controllers/PlaylistController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDBProject.DataAccess;
+
+namespace MongoDBProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+        private readonly IPlaylistRepository _repository;
+
+        public PlaylistController(IPlaylistRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public IActionResult Get() => Ok(_repository.GetAll());
+
+        // will be continued...
+    }
+}
