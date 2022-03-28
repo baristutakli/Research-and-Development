4c3295302662e33b1b47421b1b5f2aa8d28682a5
kodluyoruz <baristutakli@hotmail.com>
2022-03-28 20:27:58 +0300

    MongoContext

---
+++ src/NOSQL/MongoDBProject/Common/Context/IMongoContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDBProject.Common.Context
+{
+    public interface IMongoContext
+    {
+        IMongoCollection<T> GetCollection<T>(string name);
+    }
+}
---
+++ src/NOSQL/MongoDBProject/Common/Context/MongoContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using MongoDBProject.Common.Interfaces;
+
+
+namespace MongoDBProject.Common.Context
+{
+    public class MongoContext
+    {
+        private readonly MongoClient _dBClient;
+        private readonly IMongoDatabase _db;
+        public MongoContext(IMongoDBSettings settings)
+        {
+            _dBClient = new MongoClient(settings.ConnectionURI);
+            _db = _dBClient.GetDatabase(settings.DatabaseName); 
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)=> _db.GetCollection<T>(name);
+    }
+}
