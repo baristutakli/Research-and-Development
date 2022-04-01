e50f0596a5eb2795a672632a9f1f79375f749263
baristutakli <baristutakli@hotmail.com>
2022-04-01 11:51:35 +0300

    Added Person entity and finished

---
+++ src/csharp/CQRSWithEventSourcing/Person.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+// CQRS
+// Event Sourcing
+// All the changes are encapsulates as event
+// https://www.youtube.com/watch?v=Q0Bz-O67_nI
+/**
+ * Command ~ do/change
+ * Query ~ 
+ */
+
+
+
+
+
+
+// in real world
+public class Person
+{
+    private int age;
+    EventBroker _broker;
+    public Person(EventBroker broker)
+    {
+       _broker = broker;
+        broker.Commands += BrokerOnCommands;
+        broker.Queries += BrokerOnQueries;
+    }
+
+    private void BrokerOnQueries(object? sender, Query query)
+    {
+        var ac = query as AgeQuery;
+        if (ac != null && ac.Target ==this)
+        {
+            ac.Result = age;
+        }
+    }
+
+    private void BrokerOnCommands(object? sender, Command command)
+    {
+        var cac = command as ChangeAgeCommand;
+        if (cac!=null&& cac.Target==this)
+        {
+            if (cac.Register) _broker.AllEvents.Add(new AgeChangedEvent(this, age, cac.Age));
+            age = cac.Age;
+        }
+    }
+    // additional mechanism
+    public bool CanVote => age >= 16;
+}
---
+++ src/csharp/CQRSWithEventSourcing/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// CQRS
+// Event Sourcing
+// All the changes are encapsulates as event
+// https://www.youtube.com/watch?v=Q0Bz-O67_nI
+/**
+ * Command ~ do/change
+ * Query ~ 
+ */
+Console.WriteLine("Hello, World!");
+
+
+
+
+var ebroker = new EventBroker();
+var p = new Person(ebroker
+    );
+ebroker.Command(new ChangeAgeCommand(p, 123));
+int age;
+ age= ebroker.Query<int>(new AgeQuery { Target=p});
+Console.WriteLine($"Age: {age}");
+foreach (var e in ebroker.AllEvents)
+{
+    Console.WriteLine(e);
+}
+
+ebroker.UndoLast();
+foreach (var e in ebroker.AllEvents)
+{
+    Console.WriteLine("-"+e);
+}
+age = ebroker.Query<int>(new AgeQuery { Target = p });
+Console.WriteLine($"Age: {age}");
+
+// in real world
+public class PersonStorage
+{
+    Dictionary<int, Person> people;
+}
